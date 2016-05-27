2573c222b618c28e73bc622605ef382e1c2624fe
Allan Holanda Targino <allan_targino@hotmail.com>
2016-05-27 20:42:16 -0300

    Review page gets users photo

--- ARDA/Arda.Main/Controllers/UsersController.cs
+++ ARDA/Arda.Main/Controllers/UsersController.cs
@@ -18,9 +18,52 @@ namespace Arda.Main.Controllers
     [Authorize]
     public class UsersController : Controller
     {
-        public IActionResult ReviewPermissions()
+        public async Task<IActionResult> ReviewPermissions()
         {
-            return View();
+
+            AuthenticationResult result = null;
+
+            try
+            {
+                string userObjectID = HttpContext.User.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+                AuthenticationContext authContext = new AuthenticationContext(Startup.Authority, new SessionCache(userObjectID, HttpContext));
+                ClientCredential credential = new ClientCredential(Startup.ClientId, Startup.ClientSecret);
+                result = await authContext.AcquireTokenSilentAsync(Startup.GraphResourceId, credential, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
+
+                ViewBag.Token = result.AccessToken;
+                return View();
+                //HttpClient client = new HttpClient();
+                //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/users/maluz@microsoft.com/photo/$value");
+                //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                //HttpResponseMessage response = await client.SendAsync(request);
+
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    var source = await response.Content.ReadAsByteArrayAsync();
+                //    return View();
+                //}
+                //else
+                //{
+                //    //
+                //    // If the call failed with access denied, then drop the current access token from the cache, 
+                //    //     and show the user an error indicating they might need to sign-in again.
+                //    //
+                //    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                //    {
+
+                //        //var todoTokens = authContext.TokenCache.ReadItems().Where(a => a.Resource == Startup.TodoListResourceId);
+                //        //foreach (TokenCacheItem tci in todoTokens)
+                //        //    authContext.TokenCache.DeleteItem(tci);
+
+                //        //ViewBag.ErrorMessage = "UnexpectedError";
+                //    }
+                //}
+            }
+            catch (Exception)
+            {
+                //If get silent token fails:
+                return new ChallengeResult(OpenIdConnectDefaults.AuthenticationScheme);
+            }
         }
 
         public IActionResult ListBannedUsers()
@@ -40,33 +83,33 @@ namespace Arda.Main.Controllers
                 result = await authContext.AcquireTokenSilentAsync(Startup.GraphResourceId, credential, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
 
                 ViewBag.Token = result.AccessToken;
-                
-                HttpClient client = new HttpClient();
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me/messages");
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
-                HttpResponseMessage response = await client.SendAsync(request);
+                return View();
+                //HttpClient client = new HttpClient();
+                //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/users/maluz@microsoft.com/photo/$value");
+                //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                //HttpResponseMessage response = await client.SendAsync(request);
 
-                if (response.IsSuccessStatusCode)
-                {
-                    var source = await response.Content.ReadAsByteArrayAsync();
-                    return View();
-                }
-                else
-                {
-                    //
-                    // If the call failed with access denied, then drop the current access token from the cache, 
-                    //     and show the user an error indicating they might need to sign-in again.
-                    //
-                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    {
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    var source = await response.Content.ReadAsByteArrayAsync();
+                //    return View();
+                //}
+                //else
+                //{
+                //    //
+                //    // If the call failed with access denied, then drop the current access token from the cache, 
+                //    //     and show the user an error indicating they might need to sign-in again.
+                //    //
+                //    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                //    {
 
-                        //var todoTokens = authContext.TokenCache.ReadItems().Where(a => a.Resource == Startup.TodoListResourceId);
-                        //foreach (TokenCacheItem tci in todoTokens)
-                        //    authContext.TokenCache.DeleteItem(tci);
+                //        //var todoTokens = authContext.TokenCache.ReadItems().Where(a => a.Resource == Startup.TodoListResourceId);
+                //        //foreach (TokenCacheItem tci in todoTokens)
+                //        //    authContext.TokenCache.DeleteItem(tci);
 
-                        //ViewBag.ErrorMessage = "UnexpectedError";
-                    }
-                }
+                //        //ViewBag.ErrorMessage = "UnexpectedError";
+                //    }
+                //}
             }
             catch (Exception)
             {
