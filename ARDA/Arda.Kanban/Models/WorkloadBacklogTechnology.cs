a6201efee6b9043fd16b8a64572ad60ee8b56081
Allan Holanda Targino <allan_targino@hotmail.com>
2016-06-05 18:19:02 -0300

    Workloads first commit

---
+++ ARDA/Arda.Kanban/Models/WorkloadBacklogTechnology.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arda.Kanban.Models
+{
+    public class WorkloadBacklogTechnology
+    {
+        public Guid WBUTechnologyID { get; set; }
+
+        public virtual WorkloadBacklog WorkloadBacklog { get; set; }
+
+        public Technology Technology { get; set; }
+    }
+}
---
+++ ARDA/Arda.Kanban/Models/WorkloadBacklogUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Arda.Kanban.Models
+{
+    public class WorkloadBacklogUser
+    {
+        public Guid WBUserID { get; set; }
+
+        public virtual WorkloadBacklog WorkloadBacklog { get; set; }
+
+        public UserMainViewModel User{ get; set; }
+    }
+}
