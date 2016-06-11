dff63229737f414714f5a5827d8216df3e0cb3ec
Allan Holanda Targino <allan_targino@hotmail.com>
2016-06-10 23:14:42 -0300

    File upload tests

--- ARDA/Arda.Common/ViewModels/Main/WorkloadViewModel.cs
+++ ARDA/Arda.Common/ViewModels/Main/WorkloadViewModel.cs
@@ -7,6 +7,38 @@ namespace Arda.Common.ViewModels.Main
 {
     public class WorkloadViewModel
     {
+        public Guid WBID { get; set; }
 
+        public bool WBIsWorkload { get; set; }
+        
+        public int WBStatus { get; set; }
+
+        public DateTime WBStartDate { get; set; }
+
+        public DateTime WBEndDate { get; set; }
+
+        public string WBTitle { get; set; }
+
+        public string WBDescription { get; set; }
+
+        public string WBExpertise { get; set; }
+
+        public int WBComplexity { get; set; }
+        
+        public string WBCreatedBy { get; set; }
+        
+        public DateTime WBCreatedDate { get; set; }
+
+        
+        public string WBActivity { get; set; }
+
+        public IEnumerable<string> WBFilesLinks { get; set; }
+
+        public IEnumerable<string> WBMetrics { get; set; }
+
+        public IEnumerable<string> WBUsers { get; set; }
+
+        public IEnumerable<string> WBTechnologies { get; set; }
+        
     }
 }
