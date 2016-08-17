6baf4a7a21e2c8e20f430517ddbfdf0f0435f981
Fabricio Catae <fcatae@microsoft.com>
2016-08-17 20:13:02 -0300

    publish

--- publish.cmd
+++ publish.cmd
@@ -1,11 +1,10 @@
 @ECHO OFF
 
-set PROJECTNAME=%1
+set PROJECTPATH=%1
 set WEBSITENAME=%2
 set WEBUSERNAME=%3
 set WEBPASSWORD=%4
 
-set CONTENTPATH="$(System.DefaultWorkingDirectory)/ARDA/%PROJECTNAME%/bin/Publish"
 set SERVERNAME=https://%WEBSITENAME%.scm.azurewebsites.net/msdeploy.axd?site=%WEBSITENAME%
 
-echo "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:contentPath=%CONTENTPATH% -dest:contentPath="%WEBSITENAME%",ComputerName="%SERVERNAME%",UserName="%WEBUSERNAME%",Password="%WEBPASSWORD%",IncludeAcls="False",AuthType="Basic" -enablerule:AppOffline
+"C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:contentPath="%PROJECTPATH%" -dest:contentPath="%WEBSITENAME%",ComputerName="%SERVERNAME%",UserName="%WEBUSERNAME%",Password="%WEBPASSWORD%",IncludeAcls="False",AuthType="Basic" -enablerule:AppOffline
