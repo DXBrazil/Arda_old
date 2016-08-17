126c73df0652a6eece92a757e87721efb9df1a3e
Fabricio Catae <fabricio.catae@microsoft.com>
2016-08-17 09:01:48 -0300

    fix: add constructor to DbContext

--- ARDA/Arda.Permissions/Models/PermissionsContext.cs
+++ ARDA/Arda.Permissions/Models/PermissionsContext.cs
@@ -10,6 +10,11 @@ namespace Arda.Kanban.Models
 {
     public class PermissionsContext : DbContext
     {
+        public PermissionsContext(DbContextOptions<PermissionsContext> options)
+            : base(options)
+        { }
+
+
         public DbSet<Module> Modules { get; set; }
 
         public DbSet<Resource> Resources { get; set; }
